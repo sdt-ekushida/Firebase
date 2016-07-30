2cfb684ef79a0222e7db35188a8a7f0342fb1275
e.kushida <e.kushida@smartdt.jp>
2016-07-30 18:32:59 +0900

    Get Started Sample

---
+++ 01_GetStartd/Started/Pods/Target Support Files/Pods-Started/Pods-Started-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_StartedVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StartedVersionString[];
+
---
+++ 01_GetStartd/Started/Pods/Target Support Files/Pods-StartedTests/Pods-StartedTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_StartedTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StartedTestsVersionString[];
+
